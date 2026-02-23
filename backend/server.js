7c9b2d578ece4c4833cba47f8cdb79409f49a9b7
KMSiam <k.m.siam2019@gmail.com>
2026-02-24 01:58:33 +0600

    add versel link

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://facultyreview-kr6qt7tcg-kmsiams-projects.vercel.app'],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
